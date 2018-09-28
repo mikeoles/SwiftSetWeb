4555ba378461945bb91cfc0c769d9ada65bad332
Oles <moles@ceiamerica.com>
2018-09-28 14:51:46 -0400

    Reformat

--- SwiftSetWeb/Controllers/ExercisesController.cs
+++ SwiftSetWeb/Controllers/ExercisesController.cs
@@ -115,6 +115,75 @@ namespace SwiftSetWeb.Controllers
             return PartialView("Views/Home/_PartialGrid.cshtml", currentExercises);
         }
 
+        // GET: Exercises1/Create
+        public IActionResult Create() {
+            return View();
+        }
+
+        // POST: Exercises1/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Eliminated,Name,Difficulty,Primary,Equipment,Movement,Angle,Tempo,Unilateral,Joint,Stability,Sport,Grip,Url")] Exercises exercises) {
+            if (ModelState.IsValid)
+            {
+                _context.Add(exercises);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(exercises);
+        }
+
+        // GET: Exercises1/Edit/5
+        public async Task<IActionResult> Edit(int? id) {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var exercises = await _context.Exercises.SingleOrDefaultAsync(m => m.Id == id);
+            if (exercises == null)
+            {
+                return NotFound();
+            }
+            return View(exercises);
+        }
+
+        // POST: Exercises1/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Eliminated,Name,Difficulty,Primary,Equipment,Movement,Angle,Tempo,Unilateral,Joint,Stability,Sport,Grip,Url")] Exercises exercises) {
+            if (id != exercises.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(exercises);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ExercisesExists(exercises.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(exercises);
+        }
+
         // GET: Exercises/Details/5
         public async Task<IActionResult> Details(int? id) {
             if (id == null)
