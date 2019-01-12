a94426895d6597c677bdd72b97f50200e0f18bef
Oles <moles@ceiamerica.com>
2019-01-11 23:48:29 -0500

    Put on azure

--- SwiftSetWeb/Startup.cs
+++ SwiftSetWeb/Startup.cs
@@ -26,21 +26,28 @@ namespace SwiftSetWeb
             services.AddMvc();
 
             var connection = @"Data Source =.; Initial Catalog = SwiftSet; Persist Security Info = True; User ID = SwiftSetUser; Password = sspassword!; MultipleActiveResultSets = True; Application Name = EntityFramework";
-            services.AddDbContext<SwiftSetContext>(options => options.UseSqlServer(connection));
+            // Use SQL Database if in Azure, otherwise, use SQLite
+            if (Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") == "Production")
+            {
+                services.AddDbContext<SwiftSetContext>(options =>
+                        options.UseSqlServer(Configuration.GetConnectionString("MyDbConnection")));
+            }
+            else
+            {
+                services.AddDbContext<SwiftSetContext>(options => options.UseSqlServer(connection));
+
+            }
+
+            // Automatically perform database migration
+            services.BuildServiceProvider().GetService<SwiftSetContext>().Database.Migrate();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
+
                 app.UseBrowserLink();
                 app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
-            }
 
             app.UseStaticFiles();
 
